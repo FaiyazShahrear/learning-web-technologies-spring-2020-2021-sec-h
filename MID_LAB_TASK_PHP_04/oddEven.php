f7967eb9c296e4ed521ae02811c76658cb384aca
FaiyazShahrear <shahrearfaiyaz@gmail.com>
2021-02-16 23:53:44 +0600

    3.odd or even

---
+++ MID_LAB_TASK_PHP_04/oddEven.php
@@ -0,0 +1,18 @@
+  
+<?php
+	
+	$number1 = 13;
+	$number2 = 92;
+
+	echo "Checking $number1... <br>";
+	echo "Answer: ";
+	if($number1%2 === 0) echo "Even Number";
+	else echo "Odd Number";
+
+	echo "<br>";
+	echo "Checking $number2... <br>";
+	echo "Answer: ";
+	if($number2%2 === 0) echo "Even Number";
+	else echo "Odd Number";
+
+?>
