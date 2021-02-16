9ddf1c22b96de2a0aa3e60c308339bacd9cd69ea
FaiyazShahrear <shahrearfaiyaz@gmail.com>
2021-02-16 23:47:04 +0600

    Area and perimeter

---
+++ MID_LAB_TASK_PHP_04/area.php
@@ -0,0 +1,13 @@
+<?php
+	
+	$length = 24;
+	$width = 17;
+
+	$area = $length * $width;
+	$perimeter = 2 * ($length + $width);
+
+	echo "Area of a Rectangle: $area";
+	echo "<br>";
+	echo "Perimeter of a Rectangle: $perimeter";
+
+?>
