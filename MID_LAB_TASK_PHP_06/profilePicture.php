ce38d7f43aca4a32c2dfb8bc9ea365fd9f41ea04
FaiyazShahrear <shahrearfaiyaz@gmail.com>
2021-02-25 12:24:53 +0600

    3.Profile Picture

---
+++ MID_LAB_TASK_PHP_06/profilePicture.php
@@ -0,0 +1,40 @@
+<?php
+	
+	if(isset($_POST['Submit']))
+	{
+		$supported_image = array('jpg','jpeg','png');
+		$ext = strtolower(pathinfo($_POST['ProfilePicture'], PATHINFO_EXTENSION));
+		if (in_array($ext, $supported_image) === false)
+		{
+			echo "Please check the requirement size";
+		}
+		elseif(getimagesize($_POST['ProfilePicture']) > 4)
+		{
+			echo "Please check the requirement size";
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile Picture</title>
+</head>
+<body>
+  
+	<form method="POST" action="" >
+		<fieldset>
+			<legend> <b>Profile Picture</b></legend>
+				<table>
+				<tr>
+					<td> <input type="file" name="ProfilePicture"/> 
+					<br/> </td>
+				</tr>
+			</table>
+			<hr>
+			<br> <br>
+			<input type="submit" name="Submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
