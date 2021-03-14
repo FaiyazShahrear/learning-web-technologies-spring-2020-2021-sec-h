2b7af5ea1bf24e70b56d21c15103623aa379fe6d
FaiyazShahrear <shahrearfaiyaz@gmail.com>
2021-03-14 13:11:31 +0600

    3rd commit at 1:10 PM

--- MID_LAB_EXAM/controller/regCheck.php
+++ MID_LAB_EXAM/controller/regCheck.php
@@ -1,15 +1,50 @@
 <?php
 
-if(isset($_REQUEST['submit'])){
-    
-    if(!empty($_REQUEST['name']) or !empty($_REQUEST['id']) or !empty($_REQUEST['password']) or !empty($_REQUEST['radio'])){
-        
-        if($_REQUEST['password'] == $_REQUEST['cpassword']){
-            
-           
-    
-    else{echo "Please fill up all information.";}
-    
-}
+    if(isset($_POST['submit'])){
 
+        $name = $_POST['name'];
+        $id = $_POST['id'];
+        $password = $_POST['password'];
+        $cpassword = $_POST['cpassword'];
+        $type = $_POST['type'];
+        
+
+        
+
+        if($name == "" ||$id == "" | $password == "" || $cpassword == "" ||$type == "" ){
+            echo "Please fill all information";
+
+        }else{
+
+            if($password == $cpassword ){
+                if (file_exists('../model/user.json'))
+                {
+                    $myfile= file_get_contents('../model/user.json');
+                    $current_data = json_decode($myfile,true);
+
+                       $user= [
+                                'name'         =>$name,
+                                'name'         =>$id,
+                                'password'      =>$password,
+                                'type'        =>$type
+                                ];
+                                $current_data[]=$user;
+                                $writedata=json_encode($current_data);
+                                file_put_contents('../model/user.json',$writedata);
+
+                                if (file_put_contents('../model/user.json',$writedata))
+                                {
+                                    header('location:../view/login.html');
+                                    
+                                }
+                                else{
+                                    echo "Login failed";
+                                }
+                            }
+            }
+        }
+    }
+            else{
+                echo "password & confirm password mismatch..";
+            }
 ?>
